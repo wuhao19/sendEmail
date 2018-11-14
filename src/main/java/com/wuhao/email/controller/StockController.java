4af3e2236d4d7b9df842b65922dc5ae3865b18d1
Classe <928707094@qq.com>
2018-11-14 21:46:14 +0800

    给添加商品时，设置商品的创建人和更新的人；添加自定义异常处理，和对商品的添加进行异常处理

---
+++ src/main/java/com/wuhao/email/controller/StockController.java
@@ -0,0 +1,20 @@
+package com.wuhao.email.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author wuhao
+ * @since 2018-11-12
+ */
+@RestController
+@RequestMapping("/model/stock")
+public class StockController {
+
+}
